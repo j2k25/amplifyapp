314e681074d705a2d0213af406e09b97c384e5ca
j2k25 <j2k25@users.noreply.github.com>
2022-08-26 04:51:50 -0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+		/*
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+		*/
+		<h1>Hello from V2</h1>
       </header>
     </div>
   );
