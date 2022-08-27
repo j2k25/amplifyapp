a4fbf6968e81375ba8a17548abdf7e72c1149730
j2k25 <j2k25@users.noreply.github.com>
2022-08-27 01:28:00 -0500

    React module compiled with lossed functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,123 @@
 import logo from "./logo.svg";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { API } from 'aws-amplify';
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react';
+import { listTodos } from './graphql/queries';
+import { createTodo as createNoteMutation, deleteTodo as deleteNoteMutation } from './graphql/mutations';
+import { Storage } from 'aws-amplify'; // { API, Storage }
+
+const initialFormState = { name: '', description: '' }
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+  
+  async function onChange(e) {
+	if (!e.target.files[0]) return
+	const file = e.target.files[0];
+	setFormData({ ...formData, image: file.name });
+	await Storage.put(file.name, file);
+	fetchNotes();
+  }	
+	
+  async function fetchNotes() {
+	const apiData = await API.graphql({ query: listTodos });
+	const notesFromAPI = apiData.data.listTodos.items;
+	await Promise.all(notesFromAPI.map(async note => {
+	  if (note.image) {
+		const image = await Storage.get(note.image);
+		note.image = image;
+	  }
+	  return note;
+    }))
+	setNotes(apiData.data.listTodos.items);
+  }
+  
+  
+  async function createTodo() {
+	if (!formData.name || !formData.description) return;
+	await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+	if (formData.image) {
+	  const image = await Storage.get(formData.image);
+	  formData.image = image;
+	}
+	setNotes([ ...notes, formData ]);
+	setFormData(initialFormState);
+  }
+  
+  
+  
+  
+  
+  
+/*
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listTodos });
+    setNotes(apiData.data.listTodos.items);
+  }
+
+  async function createTodo() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteTodo({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+*/
+
+  return (
+  	
+    <div className="App">
+      <h1>My Notes App</h1>
+	  <input
+        type="file"
+        onChange={onChange}
+	  />
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createTodo}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteTodo(note)}>Delete note</button>
+			  {
+				note.image && <img src={note.image} style={{width: 400}} />
+			  }
+            </div>
+          ))
+        }
+      </div>
+      <Authenticator />
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
+
+
+
+/*
 import "@aws-amplify/ui-react/styles.css";
 import {
   withAuthenticator,
@@ -41,7 +160,7 @@ function App({ signOut }) {
 export default withAuthenticator(App);
 
 
-/*
+/*----------------------------------------------------------------------------------------------------
 
 import './App.css';
 
@@ -61,7 +180,7 @@ function App() {
 
 export default App;
 
-		//------------------------------------------------
+/*------------------------------------------------
 		<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -75,4 +194,4 @@ export default App;
           Learn React
         </a>
 		</header>
-		*/
+*/
