404c34e5b80066adf14d46437cd55f638f0200bf
j2k25 <j2k25@users.noreply.github.com>
2022-08-26 21:17:47 -0500

    added auth and link

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp5d919713": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
+  return (
+    <View className="App">
+
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+		<Heading level={1}>Hello Beh Beh!</Heading>
+      </Card>
+      <Card>
+
+		<Heading level={3}>We now have Auth!</Heading>
+	  </Card>
+	  <Card>
+		<Heading level={5}>and a Link!</Heading>
+	  	<a
+          className="App-link"
+          href="https://google.com"//"https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer" //Learn React
+        >
+          Search Google 
+        </a> 
+	  </Card>
+
+		<Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
+
+export default withAuthenticator(App);
+
+
+/*
+
 import './App.css';
+
+
+
 
 function App() {
   return (
@@ -14,7 +61,7 @@ function App() {
 
 export default App;
 
-		/*
+		//------------------------------------------------
 		<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
